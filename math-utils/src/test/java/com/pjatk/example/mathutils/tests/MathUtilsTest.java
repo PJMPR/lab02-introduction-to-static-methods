09009331d9325871e33f12e76cd3c93dcfa0d204
adam urbanowicz <adam.urbanow@gmail.com>
2022-03-13 22:47:56 +0100

    add tests

--- math-utils/src/test/java/com/pjatk/example/mathutils/tests/MathUtilsTest.java
+++ math-utils/src/test/java/com/pjatk/example/mathutils/tests/MathUtilsTest.java
@@ -9,30 +9,6 @@ import static org.hamcrest.Matchers.*;
 public class MathUtilsTest {
 
     @Test
-    public void test_if_utils_properly_counts_factorial(){
-        int result = MathUtils.factorial(5);
-        assertThat(result, is(120));
-        result = MathUtils.factorialRecursive(5);
-        assertThat(result, is(120));
-        result = MathUtils.factorial(6);
-        assertThat(result, is(720));
-        result = MathUtils.factorialRecursive(6);
-        assertThat(result, is(720));
-    }
-
-    @Test
-    public void test_integrals(){
-        double[] polynomial = new double[3];
-        polynomial[0] = -1;
-        polynomial[1] = 0;
-        polynomial[2] = 1;
-
-        double result = MathUtils.integralOfPolynomial(polynomial, -1,1);
-        assertThat(result, lessThan(1.34));
-        assertThat(result, greaterThan(1.33));
-    }
-
-    @Test
     public void test_fractions(){
         String result = MathUtils.simplifyFractionInString(1,2);
         assertThat(result, equalTo("1/2"));
