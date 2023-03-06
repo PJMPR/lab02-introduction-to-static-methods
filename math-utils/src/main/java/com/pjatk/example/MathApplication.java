5d250fc246df1ab0c16ad4420174d7752f36e72c
adam urbanowicz <adam.urbanow@gmail.com>
2023-03-06 15:37:12 +0100

    poj 2023

--- math-utils/src/main/java/com/pjatk/example/MathApplication.java
+++ math-utils/src/main/java/com/pjatk/example/MathApplication.java
@@ -10,36 +10,37 @@ public class MathApplication {
         /**
          * Napisz funkcję która obliczy n!
          */
-//        int result = MathUtils.factorial(5);
-//        if(result!=120)
-//            throw new RuntimeException("Poprawny wynik 5! to 120, a tutaj wyszło "+ result);
-//        result = MathUtils.factorialRecursive(6);
-//        if(result!=720)
-//            throw new RuntimeException("Poprawny wynik 6! to 720, a tutaj wyszło "+ result);
-//        result = MathUtils.factorialRecursive(5);
-//        if(result!=120)
-//            throw new RuntimeException("(przez rekurencje)Poprawny wynik 5! to 120, a tutaj wyszło "+ result);
-//
-//        result = MathUtils.factorialRecursive(6);
-//        if(result!=720)
-//            throw new RuntimeException("(przez rekurencje)Poprawny wynik 6! to 720, a tutaj wyszło "+ result);
+        int result = MathUtils.factorial(5);
+
+        if(result!=120)
+            throw new RuntimeException("Poprawny wynik 5! to 120, a tutaj wyszło "+ result);
+        result = MathUtils.factorialRecursive(6);
+        if(result!=720)
+            throw new RuntimeException("Poprawny wynik 6! to 720, a tutaj wyszło "+ result);
+        result = MathUtils.factorialRecursive(5);
+        if(result!=120)
+            throw new RuntimeException("(przez rekurencje)Poprawny wynik 5! to 120, a tutaj wyszło "+ result);
+
+        result = MathUtils.factorialRecursive(6);
+        if(result!=720)
+            throw new RuntimeException("(przez rekurencje)Poprawny wynik 6! to 720, a tutaj wyszło "+ result);
 
         /**
          * napisz funkcję która skróci ułamki i zwróci je w postaci String
          */
 
-//        String result2 = MathUtils.simplifyFractionInString(1,2);
-//        if(!result2.equals("1/2"))
-//            throw new RuntimeException("poprawny wynik to 1/2, a tutaj wyszło "+ result2);
-//        result2 = MathUtils.simplifyFractionInString(15,20);
-//        if(!result2.equals("3/4"))
-//            throw new RuntimeException("poprawny wynik to 3/4, a tutaj wyszło "+ result2);
-//        result2 = MathUtils.simplifyFractionInString(120,5);
-//        if(!result2.equals("24"))
-//            throw new RuntimeException("poprawny wynik to 24, a tutaj wyszło "+ result2);
-//        result2 = MathUtils.simplifyFractionInString(6,4);
-//        if(!result2.equals("3/2"))
-//            throw new RuntimeException("poprawny wynik to 3/2, a tutaj wyszło "+ result2);
+        String result2 = MathUtils.simplifyFractionInString(1,2);
+        if(!result2.equals("1/2"))
+            throw new RuntimeException("poprawny wynik to 1/2, a tutaj wyszło "+ result2);
+        result2 = MathUtils.simplifyFractionInString(15,20);
+        if(!result2.equals("3/4"))
+            throw new RuntimeException("poprawny wynik to 3/4, a tutaj wyszło "+ result2);
+        result2 = MathUtils.simplifyFractionInString(120,5);
+        if(!result2.equals("24"))
+            throw new RuntimeException("poprawny wynik to 24, a tutaj wyszło "+ result2);
+        result2 = MathUtils.simplifyFractionInString(6,4);
+        if(!result2.equals("3/2"))
+            throw new RuntimeException("poprawny wynik to 3/2, a tutaj wyszło "+ result2);
 
         /**
          * napisz funkcję która policzy wartość całki oznaczonej na wielomianie
@@ -48,7 +49,7 @@ public class MathApplication {
 //        double[] polynomial = new double[3];
         /*
             zapis wielomianu: a0 + a1x + a2x^2 + ... + anx^n
-            np. dla a0=1, a1=0, a2=1
+            np. dla a0=1, a1=0, a2=-1
             będzie: 1-x^2
 
             całka z wielomianu to:
